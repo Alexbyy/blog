9483fffa6412e9e6635d49e90bbfc7d7a2d09825
baiyangyang <baiyangyang@xiaomi.com>
2019-06-17 18:09:57 +0800

    first

---
+++ controller/user.js
@@ -0,0 +1,35 @@
+const { exec, escape } = require('../db/mysql')
+const { genPassword } = require('../utils/cryp')
+
+const login = async (username, password) => {
+    username = escape(username)
+
+    // 生成加密密码
+    password = genPassword(password)
+    password = escape(password)
+
+    const sql = `
+        select username, realname from users where username=${username} and password=${password}
+    `
+    // console.log('sql is', sql)
+
+    const rows = await exec(sql)
+    return rows[0] || {}
+}
+
+const newAccount = async (username, password, realname, state) => {
+    username = escape(username)
+    realname = escape(realname)
+    state    = escape(state)
+    password = genPassword(password)
+    password = escape(password)
+    const sql = `
+        insert into users (username, password, realname, state) values (${username}, ${password}, ${realname}, ${state})
+    `
+    const rows = await exec(sql)
+    return rows[0] || {}
+}
+module.exports = {
+    login,
+    newAccount
+}
---
+++ routes/user.js
@@ -0,0 +1,41 @@
+const router = require('koa-router')()
+const { login, newAccount } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+router.prefix('/api/user')
+
+router.post('/login', async function (ctx, next) {
+    const { username, password } = ctx.request.body
+    console.log("username", username)
+    console.log("password", password)
+    const data = await login(username, password)
+    if (data.username) {
+        // 设置 session
+        ctx.session.username = data.username
+        ctx.session.realname = data.realname
+
+        ctx.body = new SuccessModel("登陆成功")
+        return
+    }
+    ctx.body = new ErrorModel('登录失败')
+})
+
+router.post('/new', async function(ctx, next){
+    const {username, password, realname, state=1} = ctx.request.body
+    const data = await newAccount(username, password, realname, state)
+    ctx.body = new SuccessModel(data)
+})
+
+// router.get('/session-test', async function (ctx, next) {
+//   if (ctx.session.viewCount == null) {
+//     ctx.session.viewCount = 0
+//   }
+//   ctx.session.viewCount++
+
+//   ctx.body ={
+//     errno: 0,
+//     viewCount: ctx.session.viewCount
+//   }
+// })
+
+module.exports = router
