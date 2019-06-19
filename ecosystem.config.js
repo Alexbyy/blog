f0fc0b02b23823d6fa052ef604b25cfa428095ce
baiyangyang <baiyangyang@xiaomi.com>
2019-06-19 17:58:56 +0800

    wad

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:Alexbyy/blog.git',
       path : '/home/alex/www/koa2',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm install && cross-env NODE_ENV=production pm2 start bin/www',
       "env"  : {
         "NODE_ENV": "production"
       }
